6f5fbb720883247bd1376dbd3f58aa7912243178
D-Belobokov <voyger0781@gmail.com>
2021-03-31 23:25:31 +0300

    Initial commit

---
+++ src/ru/synergyitacademy.java
@@ -0,0 +1,14 @@
+package ru;
+
+public class synergyitacademy {
+
+    //TODO: переписать данный код
+    // Выведется в консоль фраза Привет, мир!
+    public static void main(String[] args) {
+
+        System.out.println("Привет,мир!");
+
+    }
+
+
+}
