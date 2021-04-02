bb66f0b024cfd41cfb2d7437f7bc3ba16603dabd
D-Belobokov <81705712+D-Belobokov@users.noreply.github.com>
2021-04-02 23:28:34 +0300

    Add files via upload

---
+++ homework_2.java
@@ -0,0 +1,23 @@
+public class homework_2 {
+// выполняем инициализацию двух целочисленных переменных и замену из значения между собой
+    public static void main(String[] args) {
+int a=23 ;
+      int b=57;
+      System.out.println("переменная a="+a);
+        System.out.println("переменная b="+b);
+        System.out.println("==========");
+      a=a+b;//
+      b=a-b;//
+      a=a-b;
+        System.out.println("переменная a="+a);
+        System.out.println("переменная b="+b);
+
+
+
+    }
+}
+
+
+
+
+
